da1a10185d602435a71ba5a4b6443a983b163aa7
adam.kariv <adam.kariv@5a08bfbb-cc41-9688-f021-80dbb91975fe>
2011-03-14 15:32:26 +0000

    Apply jblebrun's patch to show source code segments

--- parse_stack.py
+++ parse_stack.py
@@ -19,22 +19,37 @@ def parsestack( lines, libname ):
 def parseasm( lines ):
     ret = []
     current = None
+    restartcode = False;
     for l in lines:
         m = funchead.match(l)
         if m:
             if current:
                 ret.append(current)
             startaddr, funcname =  m.groups()
-            current = [ funcname, int(startaddr,16), int(startaddr,16) ]
+            current = [ funcname, int(startaddr,16), int(startaddr,16), int(startaddr,16), [] ]
+            continue
         m = funcline.match(l)
         if m:
+            restartcode = True;
             addr =  m.groups()[0]
             if current != None:
-                current[2] = int(addr,16)
+                current[3] = int(addr,16)
+            continue
         m = sohead.match(l)
         if m:
             so =  m.groups()[0]
             so = os.path.split(so)[1]
+            continue
+
+        #Assume anything else is a code line
+        if restartcode:
+#            print 'XXX',l
+            restartcode = False;
+            ret.append(current)
+            current = [ current[0], current[1], current[3], current[3], [] ] 
+        if current != None:
+            current[4].append(l);
+
     return so, ret
 
 if __name__=="__main__":
@@ -42,8 +57,8 @@ if __name__=="__main__":
 
     libname, asm = parseasm( file(asm).read().split('\n') )
     stack = parsestack( file(stack).read().split('\n'), libname )
-
     for addr in stack:
-        for func, a1, a2 in asm:
-            if addr >= a1 and addr <= a2:
-                print "0x%08x:%32s + 0x%04x" % ( addr, func, addr-a1 )
+        for func, funcstart, segstart, segend, code in asm:
+            if addr >= segstart and addr <= segend:
+                print "0x%08x:%32s + 0x%04x %s" % ( addr, func, addr-funcstart, "".join(["\n"+x for x in code]))
+
